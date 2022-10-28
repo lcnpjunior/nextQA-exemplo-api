8189d7265f884c760a0a5826e74d659c932b7de8
Luiz Pereira <lcnpj@outlook.com>
2022-10-28 17:07:07 -0300

    first commit

--- tests/api_classes/lists-api.ts
+++ tests/api_classes/lists-api.ts
@@ -1,7 +1,7 @@
 import { APIRequestContext } from '@playwright/test'
 
-const API_KEY = '501564235593d291fcbac0b97cff6ba1'
-const API_TOKEN = '18f31c024e2b6cdc88f3461cf076f4ff922c7b8fb4c3e40cb4f5e9deb14eb45b' 
+const API_KEY = ''
+const API_TOKEN = '' 
 const AUTH = `OAuth oauth_consumer_key="${API_KEY}", oauth_token="${API_TOKEN}"`
 
 export class ListsAPI{
